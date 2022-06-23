e83e97dff3c07d78a05cc949b533921a2083b000
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-23 09:50:12 +0200

    implemented .env

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.scss";
 
-const url = "http://localhost:3021/jobs";
+const backend_url = "https://ejt0-jobs-app-backend.herokuapp.com/jobs";
 
 function App() {
   const [jobs, setJobs] = useState([]);
